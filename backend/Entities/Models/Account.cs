dc3b24533a5803489b3ae163f64790041c35bd5a
Kaique <kaiqsouza2004@hotmail.com>
2023-02-23 22:26:13 -0300

    teste de log, Instalação do pacote pomelo, criaçao do entities e suas models junto do RepositoryContext

---
+++ backend/Entities/Models/Account.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    [Table("Account")]
+    public class Account
+    {
+        [Key]
+        public Guid AccountId { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string AccountType { get; set; }
+
+        [ForeignKey(nameof(Owner))]
+        public Guid OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
