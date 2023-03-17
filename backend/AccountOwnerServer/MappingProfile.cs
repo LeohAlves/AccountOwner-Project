17b7bd866d7e1687253bd8d021e7e982c83ecfa2
Leonardo Alves <leonardoalves72@hotmail.com>
2023-03-16 22:24:56 -0300

    finalizaçao do backeend

---
+++ backend/AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Owner, OwnerDto>();
+
+        CreateMap<Account, AccountDto>();
+
+        CreateMap<OwnerForCreationDto, Owner>();
+
+        CreateMap<OwnerForUpdateDto, Owner>();
+    }
+}
