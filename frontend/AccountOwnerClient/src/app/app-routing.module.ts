c4f9bd9c0b38b0ee8aea6cebed08854d92b4c894
Kaique <kaiqsouza2004@hotmail.com>
2023-03-30 22:19:39 -0300

    Componente do Menu e Error, criação de rotas

--- frontend/AccountOwnerClient/src/app/app-routing.module.ts
+++ frontend/AccountOwnerClient/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
