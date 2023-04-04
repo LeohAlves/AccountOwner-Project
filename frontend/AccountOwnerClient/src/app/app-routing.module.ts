16d8349faa825c25da796a7fb1777212a8b845b6
Leonardo Alves <leonardoalves72@hotmail.com>
2023-04-04 20:34:27 -0300

    Pagina owner/list e pasta environments e _interfaces

--- frontend/AccountOwnerClient/src/app/app-routing.module.ts
+++ frontend/AccountOwnerClient/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'owner', loadChildren:()=> import('./owner/owner.module').then(m =>m.OwnerModule)},
   { path: '404', component: NotFoundComponent },
-  { path: '**', redirectTo: '/404', pathMatch: 'full' },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
 ];
 
 
--- frontend/AccountOwnerClient/src/app/app.module.ts
+++ frontend/AccountOwnerClient/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+import { OwnerModule } from './owner/owner.module';
 
 
 @NgModule({
@@ -21,7 +23,9 @@ import { NotFoundComponent } from './error-pages/not-found/not-found.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    HttpClientModule,
+    OwnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
