42ed7320e862a4826d39bef3fec7e98b364b55e2
Leonardo Alves <leonardoalves72@hotmail.com>
2023-04-11 20:30:26 -0300

    Finalização do frontend do Details de AccountOwner

--- frontend/AccountOwnerClient/src/app/shared/services/owner-repository.service.ts
+++ frontend/AccountOwnerClient/src/app/shared/services/owner-repository.service.ts
@@ -6,31 +6,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OwnerRepositoryService {
-  
+
   constructor(private http: HttpClient, private envUrl: EnvironmentUrlService) { }
-  
+
   public getOwners = (route: string) => {
     return this.http.get<Owner[]>(this.createCompleteRoute(route, this.envUrl.urlAddress));
   }
   
+  public getOwner = (route: string) => {
+    return this.http.get<Owner>(this.createCompleteRoute(route, this.envUrl.urlAddress));
+  }
+
+
   public createOwner = (route: string, owner: Owner) => {
     return this.http.post<Owner>(this.createCompleteRoute(route, this.envUrl.urlAddress),
       owner, this.generateHeaders());
   }
-  
+
   public updateOwner = (route: string, owner: Owner) => {
     return this.http.put(this.createCompleteRoute(route, this.envUrl.urlAddress), owner,
       this.generateHeaders());
   }
-  
+
   public deleteOwner = (route: string) => {
     return this.http.delete(this.createCompleteRoute(route, this.envUrl.urlAddress));
   }
-  
+
   private createCompleteRoute = (route: string, envAddress: string) => {
     return `${envAddress}/${route}`;
   }
-  
+
   private generateHeaders = () => {
     return {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
